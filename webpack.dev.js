10384285255f390f050a001fd8b468b943f16632
krisp3t <77499501+krisp3t@users.noreply.github.com>
2021-07-29 16:06:41 +0200

    Initial commit with Webpack

---
+++ webpack.dev.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+    mode: "development",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].bundle.js",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: ["postcss-preset-env"],
+                            },
+                        },
+                    },
+                    {
+                        loader: "sass-loader",
+                    },
+                ],
+            },
+        ],
+    },
+});
---
+++ webpack.prod.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = merge(common, {
+	mode: "production",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "[name].[contenthash].bundle.js",
+	},
+	plugins: [new CleanWebpackPlugin()],
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					{ loader: "style-loader" },
+					{ loader: "css-loader" },
+					{
+						loader: "postcss-loader",
+						options: {
+							postcssOptions: {
+								plugins: ["postcss-preset-env"],
+							},
+						},
+					},
+					{
+						loader: "sass-loader",
+					},
+				],
+			},
+		],
+	},
+	optimization: {
+		minimize: true,
+		minimizer: [new CssMinimizerPlugin()],
+	},
+});
