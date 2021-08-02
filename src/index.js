10e922cbfe837d8f381eab2ffa5d61046bc4764d
krisp3t <77499501+krisp3t@users.noreply.github.com>
2021-08-02 21:43:32 +0200

    Add color contrast

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import "./main.scss";
 import "./app/twemoji.js";
 import "./app/app.js";
-
-// Import SVG
-function requireAll(r) {
-	r.keys().forEach(r);
-}
-requireAll(require.context("./assets/", true, /\.svg$/)); // eslint-disable-line
