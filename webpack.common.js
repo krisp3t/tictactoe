870d7c9a88efbada230c718cbd958e6e6c54ec24
krisp3t <77499501+krisp3t@users.noreply.github.com>
2021-08-07 10:25:49 +0200

    Update webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -4,8 +4,8 @@ const SpriteLoaderPlugin = require("svg-sprite-loader/plugin");
 
 module.exports = {
 	entry: {
-		vendor: "./src/vendor.js",
 		main: "./src/index.js",
+		vendor: "./src/vendor.js",
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
@@ -28,5 +28,12 @@ module.exports = {
 				use: ["svg-sprite-loader", "svgo-loader"],
 			},
 		],
+	},
+	resolveLoader: {
+		extensions: [".ts", ".js"],
+		modules: [path.join(__dirname, "node_modules")],
+	},
+	resolve: {
+		modules: [path.join(__dirname, "node_modules")],
 	},
 };
